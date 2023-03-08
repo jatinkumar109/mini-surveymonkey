35fd57050ae4a054c2025dd8b188a274ac6ebc48
mehedi <mehedimostofa01@gmail.com>
2023-03-08 11:29:41 -0500

    Added JavaDocs to test classes

--- src/test/java/surveyMonkey/OpenEndedQuestionTest.java
+++ src/test/java/surveyMonkey/OpenEndedQuestionTest.java
@@ -2,26 +2,35 @@ package surveyMonkey;
 
 import org.junit.Before;
 import org.junit.Test;
-
-
 import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * This class tests for the functionality of the Open Ended Questions.
+ */
 public class OpenEndedQuestionTest {
     private OpenEndedQuestion openQuestion;
 
+    /**
+     * This method starts an open ended question to be tested for
+     */
     @Before
     public void initialize() {
         openQuestion = new OpenEndedQuestion("Whats your height?");
     }
 
+    /**
+     * This method checks if the answer is added to the survey properly
+     */
     @Test
     public void testAddAnswer() {
         boolean response = openQuestion.setAnswer("6ft (big man)");
         assertEquals(response, true);
     }
 
+    /**
+     * This method checks the return of a proper question type
+     */
     @Test
     public void testGetQuestionType() {
         assertEquals(openQuestion.getQuestionType(), QuestionType.OPEN_ENDED);
