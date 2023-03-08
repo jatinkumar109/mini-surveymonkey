469951d230b670cbecd9fcb53064c835688dd1c9
Mehedi Mostofa <91309756+mehedimostofa@users.noreply.github.com>
2023-03-08 15:06:47 -0500

    Not required

--- src/test/java/surveyMonkey/SurveyControllerTest.java
+++ src/test/java/surveyMonkey/SurveyControllerTest.java
@@ -1,4 +1,4 @@
-package surveyMonkey;
+//package surveyMonkey;
 import org.junit.Test;
 import org.junit.Before;
 
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * This class tests for the functionality of the SurveyMonkey Controller
- */
+
 public class SurveyControllerTest {
 
     private SurveyController controller;
@@ -18,28 +18,27 @@ public class SurveyControllerTest {
 
     /**
      * // Initialize the survey controller before test
-     */
     @Before
     public void initialize() {
         controller = new SurveyController(repository);
     }
 
-    /**
+
      * Adds a survey and tests for presence
-     */
+
     @Test
     public void testAddSurvey() {
         assertEquals(controller, true);
     }
 
-    /**
+
      * Tests for accesing a survey using its ID
      * and check its presence
-     */
+
     @Test
     public void testGetSurvey() {
         Survey surveyGet = repository.findById(1);
         assertEquals(surveyGet, true);
     }
+    */
 
-}
