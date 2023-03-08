1eaeaed81c1530037e1b231f46385c9cb42c72d8
KshitijSawhney <kshitij.saw@gmail.com>
2023-03-08 14:10:44 -0500

    Made Question.java abstract and re-introduced functionality into its children.

--- src/main/java/surveyMonkey/OpenEndedQuestion.java
+++ src/main/java/surveyMonkey/OpenEndedQuestion.java
@@ -1,6 +1,7 @@
 package surveyMonkey;
 import jakarta.persistence.*;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class handles the Open Ended type of Questions
@@ -9,28 +10,29 @@ import java.util.ArrayList;
 public class OpenEndedQuestion extends Question {
 
     @ElementCollection(targetClass=String.class)
-    private ArrayList<String> answer = new ArrayList<>();
+    private List<String> answer = new ArrayList<>();
 
     @Enumerated(EnumType.ORDINAL)
     private static final QuestionType questionType = QuestionType.OPEN_ENDED;
 
+    public OpenEndedQuestion() {
+        super();
+    }
+
     public OpenEndedQuestion(String question) {
-        super(question,questionType);
+        super(question);
     }
 
-    /**
-     * Allows the surveyor to set the answer of the open-ended question
-     */
-    public boolean setAnswers(String answer){
-       return this.answer.add(answer);
+    public List<String> getAnswers() {
+        return this.answer;
     }
 
-    /**
-     * Allows the surveyor to get the answer of the open-ended question
-     */
-    @Override
-    public ArrayList<String> getAnswers(){
-        return answer;
+    public QuestionType getQuestionType() {
+        return questionType;
+    }
+
+    public boolean setAnswer(String answer) {
+        return this.answer.add(answer);
     }
 
 }
--- src/main/java/surveyMonkey/Question.java
+++ src/main/java/surveyMonkey/Question.java
@@ -8,93 +8,39 @@ import jakarta.persistence.*;
  */
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public class Question {
+public abstract class Question {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    private final String question;
-    private final QuestionType questionType;
-    @ElementCollection(targetClass=String.class)
-    private ArrayList<String> options;
-    private int lowerBound;
-    private int upperBound;
-    @ElementCollection(targetClass=String.class)
-    private ArrayList<String> answer;
+    private String question;
 
 
-    /**
-     * constructor for open-ended question
-     * @param question strong question
-     * @param questionType enum
-     */
-    public Question(String question, QuestionType questionType) {
+    public Question() {}
+
+
+    public Question(String question) {
         this.question = question;
-        this.questionType = questionType;
     }
 
-    /**
-     * constructor for MCQ question
-     * @param question string question
-     * @param questionType enum
-     * @param options choices
-     */
-    public Question(String question, QuestionType questionType, ArrayList options) {
-        this.question = question;
-        this.questionType = questionType;
-        this.options = options;
+
+    public Integer getId() {
+        return id;
     }
 
-    /**
-     * constructor for numerical question
-     * @param question string for question string
-     * @param questionType enum
-     * @param lowerBound for answer
-     * @param upperBound for answer
-     */
-    public Question(String question, QuestionType questionType, int lowerBound, int upperBound) {
-        this.question = question;
-        this.questionType = questionType;
-        this.lowerBound = lowerBound;
-        this.upperBound = upperBound;
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
-    /**
-     * Getter the question
-     */
+
     public String getQuestion() {
         return question;
     }
 
-    /**
-     * Getter for the question type
-     */
-    public QuestionType getQuestionType() {
-        return questionType;
+
+    public void setQuestion(String question) {
+        this.question = question;
     }
-
-    /**
-     * Getter for the question options
-     */
-    public ArrayList<String> getOptions() { return options; }
-
-    /**
-     * Assigns the minimum value for the numerical range questions
-     */
-    public int getLowerBound() {
-        return lowerBound;
-    }
-
-    /**
-     * Assigns the maximum value for the numerical range questions
-     */
-    public int getUpperBound() {
-        return upperBound;
-    }
-
-    /**
-     * Allows the surveyor to get the answer of the questions
-     */
-    public ArrayList<String> getAnswers() { return answer; }
 
 }
